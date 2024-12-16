bf24be2c37a6457446e067450e43686e793aad0f
Simon-Eke <simon3eke@gmail.com>
2024-12-16 12:15:18 +0100

    Update Lab3_OrmContext.cs
    Deleted my connection string to make sure it isn't publically available information. :)

--- Data/Lab3_OrmContext.cs
+++ Data/Lab3_OrmContext.cs
@@ -38,7 +38,7 @@ public partial class Lab3_OrmContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=LAPTOP-GFMLGC10\\SQLEXPRESS;Initial Catalog=Lab3_ORM;Integrated Security=True;Trust Server Certificate=True;");
+        => optionsBuilder.UseSqlServer("Default");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
